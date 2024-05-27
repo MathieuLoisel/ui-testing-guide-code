aa5c4103b80464a432d3bbccba6df530808795e2
GROUPE\mloisel <m.loisel@cyim.com>
2024-05-27 15:22:39 +0200

    delete task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,59 +1,66 @@
 import PropTypes from "prop-types";
 export default function Task({
-  task: { id, title, state },
-  onArchiveTask,
-  onTogglePinTask,
-  onEditTitle,
-}) {
+                               task: { id, title, state },
+                               onArchiveTask,
+                               onTogglePinTask,
+                               onEditTitle,
+                               onDeleteTask,
+                             }) {
   return (
-    <div
-      className={`list-item ${state}`}
-      role="listitem"
-      aria-label={`task-${id}`}
-    >
-      <label
-        htmlFor="checked"
-        aria-label={`archiveTask-${id}`}
-        className="checkbox"
+      <div
+          className={`list-item ${state}`}
+          role="listitem"
+          aria-label={`task-${id}`}
       >
-        <input
-          type="checkbox"
-          disabled={true}
-          name="checked"
-          id={`archiveTask-${id}`}
-          checked={state === "TASK_ARCHIVED"}
-        />
-        <span
-          className="checkbox-custom"
-          onClick={() => onArchiveTask("ARCHIVE_TASK", id)}
-          role="button"
-          aria-label={`archiveButton-${id}`}
-        />
-      </label>
-
-      <label htmlFor="title" aria-label={title} className="title">
-        <input
-          type="text"
-          value={title}
-          name="title"
-          placeholder="Input title"
-          style={{ textOverflow: "ellipsis" }}
-          onChange={(e) => onEditTitle(e.target.value, id)}
-        />
-      </label>
-
-      {state !== "TASK_ARCHIVED" && (
-        <button
-          className="pin-button"
-          onClick={() => onTogglePinTask(state, id)}
-          id={`pinTask-${id}`}
-          aria-label={state === "TASK_PINNED" ? "unpin" : "pin"}
-          key={`pinTask-${id}`}
+        <label
+            htmlFor="checked"
+            aria-label={`archiveTask-${id}`}
+            className="checkbox"
         >
-          <span className={`icon-star`} />
+          <input
+              type="checkbox"
+              disabled={true}
+              name="checked"
+              id={`archiveTask-${id}`}
+              checked={state === "TASK_ARCHIVED"}
+          />
+          <span
+              className="checkbox-custom"
+              onClick={() => onArchiveTask("ARCHIVE_TASK", id)}
+              role="button"
+              aria-label={`archiveButton-${id}`}
+          />
+        </label>
+
+        <label htmlFor="title" aria-label={title} className="title">
+          <input
+              type="text"
+              value={title}
+              name="title"
+              placeholder="Input title"
+              style={{ textOverflow: "ellipsis" }}
+              onChange={(e) => onEditTitle(e.target.value, id)}
+          />
+        </label>
+        <button
+            aria-label="delete"
+            className="delete-button"
+            onClick={() => onDeleteTask(id)}
+        >
+          <span className="icon-trash" />
         </button>
-      )}
-    </div>
+        {state !== "TASK_ARCHIVED" && (
+            <button
+                className="pin-button"
+                onClick={() => onTogglePinTask(state, id)}
+                id={`pinTask-${id}`}
+                aria-label={state === "TASK_PINNED" ? "unpin" : "pin"}
+                key={`pinTask-${id}`}
+            >
+              <span className={`icon-star`} />
+            </button>
+        )}
+      </div>
   );
 }
 
@@ -73,4 +80,6 @@ Task.propTypes = {
   onTogglePinTask: PropTypes.func.isRequired,
   /** Event to change the task title */
   onEditTitle: PropTypes.func.isRequired,
+  /** Event to delete the task */
+  onDeleteTask: PropTypes.func.isRequired,
 };
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,47 +1,49 @@
 import PropTypes from "prop-types";
 import Task from "./Task";
 export default function TaskList({
-  loading,
-  tasks,
-  onTogglePinTask,
-  onArchiveTask,
-  onEditTitle,
-}) {
+                                   loading,
+                                   tasks,
+                                   onTogglePinTask,
+                                   onArchiveTask,
+                                   onEditTitle,
+                                   onDeleteTask,
+                                 }) {
   const events = {
     onTogglePinTask,
     onArchiveTask,
     onEditTitle,
+    onDeleteTask,
   };
 
   const LoadingRow = (
-    <div className="loading-item">
-      <span className="glow-checkbox" />
-      <span className="glow-text">
+      <div className="loading-item">
+        <span className="glow-checkbox" />
+        <span className="glow-text">
         <span>Loading</span> <span>cool</span> <span>state</span>
       </span>
-    </div>
+      </div>
   );
   if (loading) {
     return (
-      <div className="list-items" data-testid="loading" key={"loading"}>
-        {LoadingRow}
-        {LoadingRow}
-        {LoadingRow}
-        {LoadingRow}
-        {LoadingRow}
-        {LoadingRow}
-      </div>
+        <div className="list-items" data-testid="loading" key={"loading"}>
+          {LoadingRow}
+          {LoadingRow}
+          {LoadingRow}
+          {LoadingRow}
+          {LoadingRow}
+          {LoadingRow}
+        </div>
     );
   }
   if (tasks.length === 0) {
     return (
-      <div className="list-items" key={"empty"} data-testid="empty">
-        <div className="wrapper-message">
-          <span className="icon-check" />
-          <p className="title-message">You have no tasks</p>
-          <p className="subtitle-message">Sit back and relax</p>
+        <div className="list-items" key={"empty"} data-testid="empty">
+          <div className="wrapper-message">
+            <span className="icon-check" />
+            <p className="title-message">You have no tasks</p>
+            <p className="subtitle-message">Sit back and relax</p>
+          </div>
         </div>
-      </div>
     );
   }
   const tasksInOrder = [
@@ -50,17 +52,17 @@ export default function TaskList({
   ];
 
   return (
-    <div
-      className="list-items"
-      data-testid="success"
-      key={"success"}
-      role="list"
-      aria-label="tasks"
-    >
-      {tasksInOrder.map((task) => (
-        <Task key={task.id} task={task} {...events} />
-      ))}
-    </div>
+      <div
+          className="list-items"
+          data-testid="success"
+          key={"success"}
+          role="list"
+          aria-label="tasks"
+      >
+        {tasksInOrder.map((task) => (
+            <Task key={task.id} task={task} {...events} />
+        ))}
+      </div>
   );
 }
 TaskList.propTypes = {
@@ -69,6 +71,7 @@ TaskList.propTypes = {
   onTogglePinTask: PropTypes.func.isRequired,
   onArchiveTask: PropTypes.func.isRequired,
   onEditTitle: PropTypes.func.isRequired,
+  onDeleteTask: PropTypes.func.isRequired,
 };
 
 TaskList.defaultProps = {
